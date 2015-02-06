cf795c55f915892140b04a51b183c5b8f3543fc4
millionhari <mikemhari@gmail.com>
2015-02-06 12:04:43 -0800

    Finish set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,33 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set.storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this.storage.push(item);
 };
 
 setPrototype.contains = function(item){
+  var result = false;
+  _.each(this.storage, function(element, index, list){
+    if (element === item){
+      result = true;
+    }
+  })
+  return result;
 };
 
 setPrototype.remove = function(item){
+  var target;
+  _.each(this.storage, function(element, index){
+    if (element === item){
+      target = index;
+    }
+  })
+  return this.storage.splice(target, 1);
 };
 
 /*
