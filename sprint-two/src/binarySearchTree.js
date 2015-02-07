461585be46f0bb6ac21ddfab6b9928c4fdf8c91d
millionhari <mikemhari@gmail.com>
2015-02-06 19:24:06 -0800

    Finish binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -24,11 +24,33 @@ BinarySearchTree.prototype.insert = function(value){
 };
 
 BinarySearchTree.prototype.contains = function(value){
+  //if this.value = value // return true
+    if(this.value === value){
+      return true;
+    }
+    //if value < this.value
+    if(value < this.value && this.left !== null){
+      //return contains on this.left
+        return this.left.contains(value);
+    }
+    if(this.left === null){
+      return false;
+    }
 
+    //if value > this.value
+    if(value > this.value && this.right !== null){
+      //return contains on this.left
+        return this.right.contains(value);
+    }
+    if(this.right === null){
+      return false;
+    }
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(cb){
-
+  cb(this.value);
+  if (this.left) this.left.depthFirstLog(cb);
+  if (this.right) this.right.depthFirstLog(cb);
 };
 
 //worth some hi-chews
