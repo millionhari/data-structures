b9415774b2dcc9e23e8920aa6a507fb2df6d19ec
millionhari <mikemhari@gmail.com>
2015-02-06 16:41:03 -0800

    Finish insert of binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,45 @@
 var BinarySearchTree = function(value){
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
+BinarySearchTree.prototype.insert = function(value){
+  //add to left
+  if(value < this.value){
+    if(this.left === null){
+      this.left = new BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  }
+  //add to right
+  if(value > this.value){
+    if(this.right === null){
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
 
+BinarySearchTree.prototype.contains = function(value){
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb){
+
+};
+
+//worth some hi-chews
+BinarySearchTree.prototype.breadthFirstLog = function(cb){
+
+};
+
+//worth lots of hi-chews
+BinarySearchTree.prototype.rebalanceTree = function(){
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
