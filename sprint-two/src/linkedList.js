c2674297c7fb07e5674dde6ba9e9417eb305f1dd
millionhari <mikemhari@gmail.com>
2015-02-06 21:04:57 -0800

    Refactor LinkedList addToTail

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,42 +2,24 @@
   var list = {};
   list.head = null;
   list.tail = null;
-  var tailIndex = 0;
-  var headIndex = 0;
+
 
   list.addToTail = function(value){
-    //Create node at current tail
-    list[tailIndex] = Node(value);
-    list.tail = Node(value);
+    if(list.head === null) list.head = Node(value);
 
-    if (list.head === null){
-      list[headIndex] = Node(value);
-      list.head = Node(value);
-    }
+    if(list.tail === null) list.tail = Node(value);
 
-    //If list has more than 1 node, assign pointer to next node
-    if (tailIndex - headIndex > 0 ){
-      list[tailIndex-1].next = list[tailIndex];
-    }
-    //Go to next tail
-    tailIndex++;
+    var newNode = Node(value);
+    list.tail.next = newNode;
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
-    var result = list[headIndex];
-    delete list[headIndex];
-    headIndex++;
-    list.head = list[headIndex];
-    return result.value;
+
   };
 
   list.contains = function(target){
-    for(var key in list){
-      if(list[key]['value'] === target){
-        return true;
-      }
-    }
-    return false;
+
   };
 
   return list;
