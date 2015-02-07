5fee1054b64a6ec332cf7dfa63d6816e6664d1ac
millionhari <mikemhari@gmail.com>
2015-02-07 11:42:01 -0800

    Refactor list.contains

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,21 +5,46 @@
 
 
   list.addToTail = function(value){
-    if(list.head === null) list.head = Node(value);
-
-    if(list.tail === null) list.tail = Node(value);
-
     var newNode = Node(value);
+    if(list.head === null && list.tail === null){
+      list.head = newNode;
+      list.tail = newNode;
+      return;
+    }
+
     list.tail.next = newNode;
     list.tail = newNode;
   };
 
   list.removeHead = function(){
-
+    //store value of head
+    var result = list.head.value;
+    //reassign head to next node
+    list.head = list.head.next;
+    //return value of previous head
+    return result;
   };
 
   list.contains = function(target){
+    //look to see if next node is null
+      if (this.head){
+        return this.head.contains;
+      } else {
+        if(this.next === null){
+         // return false if true
+         return false;
+        }
+        //compare value to target
+        if(target === this.value){
+         //return true if found
+         return true;
+        } else {
+          return this.next.contains(target);
+        }
+      }
+    //else
 
+      //recurse on next node
   };
 
   return list;
